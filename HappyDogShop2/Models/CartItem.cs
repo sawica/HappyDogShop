b7a51edddb6f32a48af7607689290bc55b0c511f
patrycjarecko <patrycjarecko00@gmail.com>
2021-11-18 02:11:01 +0100

    feat: add CartItem and Order models

---
+++ HappyDogShop2/Models/CartItem.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HappyDogShop2.Models
+{
+    public class CartItem
+    {
+        public int ItemId { get; set; }
+        [ForeignKey("Product"), Display(Name = "Produkt")]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        [Required(ErrorMessage = "Wybierz między 1 a 15"), Display(Name = "Ilość"), Range(1, 15)]
+        public int Quantity  { get; set; }
+    }
+}
---
+++ HappyDogShop2/Models/Order.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HappyDogShop2.Models
+{
+    public enum Status
+    {
+        Przyjęte, Zapłacone, Wysłane, Dostarczone
+    }
+    public class Order
+    {
+        public int OrderId { get; set; }
+        
+        [ForeignKey("User"), Required(ErrorMessage = "To pole jest wymagane")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        
+        [Required(ErrorMessage = "To pole jest wymagane"), Display(Name = "Adres")]
+        public string Adress { get; set; }
+        [Required(ErrorMessage = "To pole jest wymagane"), Display(Name = "Miasto")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "To pole jest wymagane"), Display(Name = "Kraj")]
+        public string Country { get; set; }
+        [Required(ErrorMessage = "To pole jest wymagane"), DataType(DataType.PostalCode), Display(Name = "Kod pocztowy")]
+        public string ZipCode { get; set; }
+        [Required(ErrorMessage = "To pole jest wymagane"), Display(Name = "Suma")]
+        public decimal AmountPaid { get; set; }
+        [Required(ErrorMessage = "To pole jest wymagane"), DataType(DataType.PhoneNumber), Display(Name = "Numer telefonu")]
+        public string Phone_number { get; set; }
+        [DisplayFormat(DataFormatString = "Przyjęte")]
+        public Status Status { get; set; }
+        
+        
+        public List<CartItem> CartItem { get; set; }
+    }
+}
