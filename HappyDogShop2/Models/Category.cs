9079f04a1d1f079eb16ce314dd949c31050fd5aa
patrycjarecko <patrycjarecko00@gmail.com>
2021-11-18 03:53:10 +0100

    feat: add fk to models, change display names

--- HappyDogShop2/Models/Category.cs
+++ HappyDogShop2/Models/Category.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
@@ -11,14 +12,19 @@ namespace HappyDogShop2.Models
 public class Category
     {
         public int CategoryId { get; set; }
-        public int? ParentId { get; set; }
-        [Required(ErrorMessage = "Name is required")]
+        [Required(ErrorMessage = "To pole jest wymagane"),Display(Name = "Nazwa")]
         public string Name { get; set; }
-        [DefaultValue(false), Display(Name = "Is hidden")]
+        [DefaultValue(false), Display(Name = "Ukryty")]
         public bool Is_hidden { get; set; }
-
+        
+        [ForeignKey("Parent"), Display(Name = "Rodzic")]
+        public int? ParentId { get; set; }
         public virtual Category Parent { get; set; }
+        
+        [ForeignKey("Sale"), Display(Name = "Promocja")]
+        public int? SaleId { get; set; }
         public virtual Sale Sale { get; set; }
+        
         public virtual ICollection<Category> Children { get; set; }
         public virtual ICollection<Product> Products { get; set; }
     }
