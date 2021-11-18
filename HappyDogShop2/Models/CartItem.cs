0fb49daa448affae6bb8c91fa4f5c1e03a2d60d5
patrycjarecko <patrycjarecko00@gmail.com>
2021-11-18 03:06:30 +0100

    refactor: delete rows

--- HappyDogShop2/Models/CartItem.cs
+++ HappyDogShop2/Models/CartItem.cs
@@ -6,10 +6,11 @@ namespace HappyDogShop2.Models
     public class CartItem
     {
         public int ItemId { get; set; }
+        [Required(ErrorMessage = "Wybierz między 1 a 15"), Display(Name = "Ilość"), Range(1, 15)]
+        public int Quantity  { get; set; }
+        
         [ForeignKey("Product"), Display(Name = "Produkt")]
         public int ProductId { get; set; }
         public Product Product { get; set; }
-        [Required(ErrorMessage = "Wybierz między 1 a 15"), Display(Name = "Ilość"), Range(1, 15)]
-        public int Quantity  { get; set; }
     }
 }
