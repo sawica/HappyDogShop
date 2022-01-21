c6db7b3d1e8378ed9c21c24bfc20b22504b94a5e
patrycjarecko <patrycjarecko00@gmail.com>
2022-01-22 00:24:21 +0100

    feat: new configurations

--- HappyDogShop2/Migrations/Configuration.cs
+++ HappyDogShop2/Migrations/Configuration.cs
@@ -1,23 +1,16 @@
-﻿namespace HappyDogShop2.Migrations
-{
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
 
+namespace HappyDogShop2.Migrations
+{
     internal sealed class Configuration : DbMigrationsConfiguration<HappyDogShop2.Models.MyDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
-
-        protected override void Seed(HappyDogShop2.Models.MyDbContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
-        }
-    }
+    } 
 }
