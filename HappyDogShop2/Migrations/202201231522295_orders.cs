76e69474ce9104c19aa4ed9b0e322b77aa4f2c76
patrycjarecko <patrycjarecko00@gmail.com>
2022-01-23 16:29:08 +0100

    feat: migrations

---
+++ HappyDogShop2/Migrations/202201231522295_orders.cs
@@ -0,0 +1,52 @@
+﻿namespace HappyDogShop2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class orders : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CartItems",
+                c => new
+                    {
+                        ItemId = c.Int(nullable: false, identity: true),
+                        Quantity = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        OrderId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ItemId)
+                .ForeignKey("dbo.Orders", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId)
+                .Index(t => t.OrderId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        AmountPaid = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Status = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CartItems", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.CartItems", "OrderId", "dbo.Orders");
+            DropForeignKey("dbo.Orders", "UserId", "dbo.Users");
+            DropIndex("dbo.Orders", new[] { "UserId" });
+            DropIndex("dbo.CartItems", new[] { "OrderId" });
+            DropIndex("dbo.CartItems", new[] { "ProductId" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.CartItems");
+        }
+    }
+}
