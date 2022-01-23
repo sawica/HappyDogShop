23d7362dc009e5f0f9dee74f2f98ed554aad95b6
patrycjarecko <patrycjarecko00@gmail.com>
2022-01-23 17:29:56 +0100

    feat: add cartItem views and controller

---
+++ HappyDogShop2/Controllers/CartItemController.cs
@@ -0,0 +1,133 @@
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using HappyDogShop2.Models;
+
+namespace HappyDogShop2.Controllers
+{
+    public class CartItemController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+
+        // GET: CartItems
+        public ActionResult Index(int OrderId = -1)
+        {
+            List<CartItem> list= new List<CartItem>();
+            if (OrderId != -1)
+            {
+                list = db.CartItems.Where(cartItem => cartItem.OrderId == OrderId).ToList();
+            }
+            else
+            {
+                list = db.CartItems.ToList();
+            }
+            return View(list);
+        }
+
+        // GET: CartItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CartItem cartItem = db.CartItems.Find(id);
+            if (cartItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CartItems/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemId,Quantity,ProductId, OrderId")] CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CartItems.Add(cartItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CartItem cartItem = db.CartItems.Find(id);
+            if (cartItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemId,Quantity,ProductId, OrderId")] CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cartItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CartItem cartItem = db.CartItems.Find(id);
+            if (cartItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CartItem cartItem = db.CartItems.Find(id);
+            db.CartItems.Remove(cartItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
