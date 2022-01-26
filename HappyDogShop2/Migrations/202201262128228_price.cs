b8a0973964eb987da36262b0020b4a5c4d4bc7ff
patrycjarecko <patrycjarecko00@gmail.com>
2022-01-26 22:29:46 +0100

    feat: migration

---
+++ HappyDogShop2/Migrations/202201262128228_price.cs
@@ -0,0 +1,18 @@
+﻿namespace HappyDogShop2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class price : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Products", "Price", c => c.Single(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "Price", c => c.Int(nullable: false));
+        }
+    }
+}
