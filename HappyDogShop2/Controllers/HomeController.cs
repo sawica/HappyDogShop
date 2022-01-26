b92643846c4f95c971a8961d1daa462f227b5630
patrycjarecko <patrycjarecko00@gmail.com>
2022-01-26 23:20:21 +0100

    feat: best products

--- HappyDogShop2/Controllers/HomeController.cs
+++ HappyDogShop2/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HappyDogShop2.Models;
 
 namespace HappyDogShop2.Controllers
 {
     public class HomeController : Controller
     {
+        private MyDbContext db = new MyDbContext();
+
         public ActionResult Index()
         {
+            List<int> list = new List<int>();
+            list = db.CartItems.GroupBy(item => item.Product.ProductId).OrderByDescending(item => item.Count()).Select(x=>x.Key).ToList().GetRange(0,3);
+            ViewBag.BestProducts = db.Products.Where(x => list.Contains(x.ProductId));
+            Console.Write(list.Count);
             return View();
         }
 
